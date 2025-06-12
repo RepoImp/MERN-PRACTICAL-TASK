febb4241b63c3e920366081cbeebfb7bfb23509c
harshal255 <200280107119@ldceahm.gujgov.edu.in>
2025-06-13 01:53:22 +0530

    only frontend code on frontend branch

--- server/config/index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const config = {
-  port: process.env.PORT,
-  mongodb_uri: process.env.MONGODB_URI,
-  jwt_token_secret: process.env.JWT_TOKEN_SECRET,
-};
-
-module.exports = config;
--- server/validation-schemas/userSchema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require("joi");
-
-const registerUserSchema = Joi.object().keys({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(8).required(),
-});
-
-module.exports = registerUserSchema;
